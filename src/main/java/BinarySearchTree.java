184f0913dcc83c372f93b60fced04a3b51580201
Hanane8 <hanane.mezzine@gmail.com>
2017-03-28 13:07:01 +0200

    Adds the BinarySearchTree class

---
+++ src/main/java/BinarySearchTree.java
@@ -0,0 +1,125 @@
+import java.util.Iterator;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class BinarySearchTree<K>
+        implements Tree<K, Node<K>>, Iterable<Node<K>> {
+    private Node<K> root;
+
+    @Override
+    public void insert(K key) {
+    }
+
+    @Override
+    public void delete(K key) {
+    }
+
+    @Override
+    public Node<K> find(K key) {
+        return null;
+    }
+
+    @Override
+    public Node<K> getSuccessor(K key) {
+        return null;
+    }
+
+    @Override
+    public Node<K> getPredecessor(K key) {
+        return null;
+    }
+
+    @Override
+    public Node<K> getMinimum() {
+        return null;
+    }
+
+    @Override
+    public Node<K> getMaximum() {
+        return null;
+    }
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
+
+    @Override
+    public Iterator<Node<K>> iterator() {
+        return new InOrderIterator();
+    }
+
+    public Iterator<Node<K>> getPreOrderIterator() {
+        return null;
+    }
+
+    public Iterator<Node<K>> getPostOrderIterator() {
+        return null;
+    }
+
+
+    @Getter
+    @Setter
+    private class BstNode<K> extends Node<K> {
+        private BstNode<K> leftChild;
+        private BstNode<K> rightChild;
+        private BstNode<K> parent;
+
+        public BstNode() {
+        }
+
+        public BstNode(K key) {
+            super(key);
+        }
+
+        @Override
+        public int getHeight() {
+            return 0;
+        }
+        @Override
+        public int getDepth() {
+            return 0;
+        }
+    }
+
+    private class InOrderIterator
+            implements Iterator<Node<K>> {
+
+        public boolean hasNext() {
+            return false;
+        }
+
+        public Node<K> next() {
+            return null;
+        }
+    }
+
+    private class PreOrderIterator
+            implements Iterator<Node<K>> {
+
+        public boolean hasNext() {
+            return false;
+        }
+
+        public Node<K> next() {
+            return null;
+        }
+
+    }
+
+    private class PostOrderIterator
+            implements Iterator<Node<K>> {
+
+        public boolean hasNext() {
+            return false;
+        }
+
+        public Node<K> next() {
+            return null;
+        }
+
+    }
+
+
+}
